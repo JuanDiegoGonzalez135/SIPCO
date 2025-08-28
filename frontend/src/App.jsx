ae7f7054be60ed0b345a7bbbcec323b9e96012d7
JuanDiegoGonzalez135 <133397601+JuanDiegoGonzalez135@users.noreply.github.com>
2025-08-28 16:17:42 -0600

    [FIRT]- Subida inicial del proyecto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Bienvenido a SIPCO</h1>
+      <p>Sistema personal contable</p>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',  // Asegúrate de que las rutas estén correctas para tus archivos
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
